60e8b1db9936b6b522f7904523550a7cbda8ad78
Chris Eaby <62377152+Christopher-Eaby@users.noreply.github.com>
2021-01-25 13:48:19 +0200

    hosting
    script now hosts the html file to the network

--- mapping/mapping.py
+++ mapping/mapping.py
@@ -14,6 +14,9 @@ Contact :
 import folium
 import pandas as pd
 import pymongo
+import http.server
+import socketserver
+
 
 #creates mongodatabase and the collections
 myclient = pymongo.MongoClient("mongodb+srv://Arthur:102030_art@landminecluster.ia7dg.mongodb.net/test")
@@ -62,3 +65,18 @@ for index, row in data.iterrows():
     
 map1.add_child(folium.LayerControl())
 map1.save("mines.html") 
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'mines.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
+
+PORT = 8000
+my_server = socketserver.TCPServer(("", PORT), handler_object)
+
+# Star the server
+my_server.serve_forever()

ef17995bdad781c11ee4ed890173e3ef2b793c97
Chris Eaby <62377152+Christopher-Eaby@users.noreply.github.com>
2021-02-10 10:45:41 +0200

    most recent changes

--- server/robotonusServer(Alt).js
+++ server/robotonusServer(Alt).js
@@ -1,17 +1,48 @@
-var io = require('socket.io')(http) 
+//var io = require('socket.io')(http) 
 var http = require('http');
 var fs = require('fs');
+var events = require('events');
+//var PythonShell = require('python-shell');
+//var pyshell = PythonShell(motorMovement);
+
+const express = require('express')
+const app = express()
+const {spawn} = require('child_process')
+const python = spawn('python', ['script1.py','node.js','python']);
+var pyMotionArr = ['left.py', 'right.py', 'forward.py', 'backward.py'];
+lenArr=pyMotionArr.length
+//for (i=0; i < lenArray; i++) {
+   //if (pyMotionArray[i]==) {
+
+//   }
+//}
+const childPython = spawn('python', ['']);
+
+childPython.stdout.on('data', (data) => {
+    console.log('stdout: ${data}');
+});
+
+childPython.stderr.on('data', (data) => {
+  console.log('stderr: ${data}');
+});
+
+childPython.stdout.on('close', (data) => {
+  console.log('child process exited with code ${data}');
+});
+
+app.post('/instructions', function(reg, pes){
+     
+
+});
+app.listen(1738);
+
 http.createServer(function (req, res) {
   fs.readFile('Robotonus_index.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
+     res.writeHead(200, {'Content-Type': 'text/html'});
+     res.write(data);
+     return res.end();
   });
+}).listen(4200);
 
-  fs.readFile('Movement.py', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/python'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
 
+   
